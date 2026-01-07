9af76b38b89177dea460e4cf4a73e85404a8fe6b
graceliu-b <grace_liu@horacemann.org>
2026-01-07 12:20:26 -0500

    src

---
+++ src/App.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to the Number Guess Game!");
+        // See Game for pseudocode you need to fill out
+        // Create a new Game that sets a number within the user's specified number range
+        ArrayList<Game> games = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("To play a game, press 'y'. If you want to stop playing, type anything other than 'y'.");
+        while (sc.hasNext() && sc.next().equals("y")) {
+            Game g = new Game(1, 100);
+            g.play();
+            games.add(g);
+            System.out.println("press 'y' to play another game.");
+        }
+
+    }
+
+    public static void getBestGame(ArrayList<Game> games) {
+        // best game
+        int minGame = 0;
+        for (Game g : games) {
+            if (g.getNumGuesses() < minGame) {
+                minGame = g.getNumGuesses();
+            }
+        }
+        System.out.println("min game is: " + minGame);
+    }
+}
---
+++ src/Game.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Game {
+    int guesses;
+    int numToGuess;
+    static Scanner sc = new Scanner(System.in);
+    ArrayList<Integer> previousGuesses = new ArrayList<>();
+    int[]prevGuesses;
+
+    Game(int low, int high) {
+        low = 0;
+        high = 100;
+        guesses = 0;
+        System.out.println("I'm thinking of a number " + low + " to " + high);
+        Random r = new Random();
+        numToGuess = r.nextInt(low,high);
+
+    }
+
+    void play() {
+        System.out.println("begin play!");
+        int guess = getGuess();
+        // gets the user guess by calling getGuess()
+       
+        while(guess != numToGuess){
+             System.out.println("You guessed " + guess);
+        
+             if(guess<numToGuess){
+                System.out.println("The number is higher!");}
+            if(guess>numToGuess){
+                System.out.println("The number is lower!");
+            }
+            guess = getGuess();
+             }
+         System.out.println("Done playing!");
+            System.out.println("You guessed the number in " + (guesses) + "guesses!");
+    }
+
+    int getGuess() { //in this function make an ArrayList and do task 2
+        if(sc.hasNextInt()){
+            int guess = sc.nextInt();
+             if(previousGuesses.contains(guess)) {
+                System.out.println("You already guessed this!");
+                return getGuess();
+            }
+        previousGuesses.add(guess);
+        guesses++;
+        return guess;
+        }  else{
+            System.out.println("Please type in an integer");
+            sc.next();
+            return getGuess();
+        }
+    }
+
+    int getNumGuesses() {
+        return guesses;
+    }
+
+}
+
